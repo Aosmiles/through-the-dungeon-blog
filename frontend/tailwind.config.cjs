6ac3dc6bd8cafe01b14a3406a8b98f9561dee1c5
Aosmiles <48396670+Aosmiles@users.noreply.github.com>
2023-03-10 21:57:53 -0800

    start styling

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -3,9 +3,24 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     colors: {
+      "riso-red": "#FF665E",
       "riso-blue": "#0078BF",
+      "riso-green": "#00A95C",
+      "riso-purple": "#765BA7",
+      "riso-yellow": "#FFE800",
+      "riso-teal": "#00838A",
+      "riso-black": "#000000",
     },
-    extend: {},
+
+    extend: {
+      borderWidth: {
+        10: "10px",
+        16: "16px",
+      },
+      fontFamily: {
+        display: ["Jost", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
