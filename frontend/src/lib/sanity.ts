640c31b3d78a63a77c94b849efe39f78db6af450
Aosmiles <48396670+Aosmiles@users.noreply.github.com>
2023-03-14 22:08:16 -0700

    adjust image sizing

--- frontend/src/lib/sanity.ts
+++ frontend/src/lib/sanity.ts
@@ -1,9 +1,26 @@
 import { useSanityClient, groq } from "astro-sanity";
 
 export async function getAllPosts() {
-  const query = groq`*[_type == "post"] | order(_createdAt desc)`;
+  const query = groq`*[_type == "post"]{
+  ...,
+   illustration->{
+    ...,
+    "size": image.asset->metadata.dimensions
+  }
+}
+| order(_createdAt desc)`;
   const allPosts = await useSanityClient().fetch(query);
   return allPosts;
+}
+
+export async function getAllIllustrations() {
+  const query = groq`*[_type == "illustration"] {
+  ...,
+  "size": image.asset->metadata.dimensions
+}
+| order(_createdAt desc)`;
+  const illustrations = await useSanityClient().fetch(query);
+  return illustrations;
 }
 
 export async function getSiteSettings() {
