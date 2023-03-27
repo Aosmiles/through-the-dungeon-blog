b573ae7f1bd3a2abae1d25c3aae440fa00ead0df
Aosmiles <arlin.smiles@gmail.com>
2023-03-27 10:42:06 -0700

    hide gallery items untill layout is loaded

--- frontend/src/lib/masonry.js
+++ frontend/src/lib/masonry.js
@@ -1,8 +1,19 @@
 import MiniMasonry from "minimasonry";
 
+const gallery = document.querySelector(".masonry");
+const galleryItem = document.querySelectorAll(".masonry-item");
+
 const masonry = new MiniMasonry({
   container: ".masonry",
   baseWidth: 200,
   gutter: 15,
   ultimateGutter: 10,
 });
+
+gallery.classList.replace("grid", "relative");
+
+galleryItem.forEach((item) => {
+  item.classList.replace("hidden", "absolute");
+});
+
+masonry.layout();
