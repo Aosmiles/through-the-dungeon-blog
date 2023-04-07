1bb3a7d0214d2eca3ec4421a6fc31d8687ffa7d4
Aosmiles <arlin.smiles@gmail.com>
2023-04-07 16:13:02 -0700

    change sanity theme

--- backend/sanity.config.ts
+++ backend/sanity.config.ts
@@ -1,3 +1,4 @@
+import {theme} from 'https://themer.sanity.build/api/hues?default=465f2d;600&primary=047c9c&transparent=465835;600&positive=43d675;300&caution=fbd024;200&lightest=fcfdfc&darkest=11150d'
 import {defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
@@ -5,6 +6,7 @@ import {schemaTypes} from './schemas'
 import {myStructure} from './src/structure'
 
 export default defineConfig({
+  theme,
   name: 'default',
   title: 'through the dungeon',
 
