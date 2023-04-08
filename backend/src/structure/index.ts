a8f8363ac7191e5db3c39a947fbc24eb9096cca2
Aosmiles <arlin.smiles@gmail.com>
2023-04-07 17:58:59 -0700

    add divider

--- backend/src/structure/index.ts
+++ backend/src/structure/index.ts
@@ -5,6 +5,7 @@ export const myStructure = (S: any) =>
       S.listItem()
         .title('Site Settings')
         .child(S.document().schemaType('siteSettings').documentId('siteSettings')),
+      S.divider(),
       ...S.documentTypeListItems().filter(
         (listItem: any) => !['siteSettings'].includes(listItem.getId())
       ),
